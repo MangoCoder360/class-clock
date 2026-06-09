441719c41fcbf2845c5b746053a005aba290caa0
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2026-06-09 09:42:39 -0700

    finals schedule

--- script.js
+++ script.js
@@ -18,20 +18,9 @@ const demoSchedule = [
 
 const whsTueThruFriSchedule = [
   { period: 'Before School', start: '08:30', end: '09:00' },
-  { period: 'Period 1', start: '09:00', end: '09:55' },
-  { period: 'Passing Time', start: '09:55', end: '10:00' },
-  { period: 'Period 2', start: '10:00', end: '10:54' },
-  { period: 'Passing Time', start: '10:54', end: '10:59' },
-  { period: 'Period 3', start: '10:59', end: '11:53' },
-  { period: 'Lunch', start: '11:53', end: '12:23' },
-  { period: 'Passing Time', start: '12:23', end: '12:28' },
-  { period: 'Period 4', start: '12:28', end: '13:22' },
-  { period: 'Passing Time', start: '13:22', end: '13:27' },
-  { period: 'GRIT 101', start: '13:27', end: '13:52' },
-  { period: 'Passing Time', start: '13:52', end: '13:57' },
-  { period: 'Period 5', start: '13:57', end: '14:51' },
-  { period: 'Passing Time', start: '14:51', end: '14:56' },
-  { period: 'Period 6', start: '14:56', end: '15:50' },
+  { period: 'Period 1/2/3 Final', start: '09:00', end: '10:35' },
+  { period: 'Period 4/5/6 Final', start: '10:40', end: '12:15' },
+  { period: 'Lunch', start: '12:15', end: '12:50' }
 ];
 
 const whsMondaySchedule = [
